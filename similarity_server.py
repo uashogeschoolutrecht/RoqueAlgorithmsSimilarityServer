d458fe006b613066cc6a3b1f934eb5fec9c068ad
LibrechtKuijvenhoven <librecht@live.nl>
2023-01-09 14:57:44 +0100

    delete test print

--- similarity_server.py
+++ similarity_server.py
@@ -25,7 +25,6 @@ def get_similarity():
         Returns:
             string: the similarity score as a string, because flask doesnt want to return a float..
     """
-    print(request.form)
     # the parameters can be accessed through `request.form[{parameter name}]`
     original_article = {"article": request.form["original_article"],
                         "language": request.form["original_language"] if request.form[
